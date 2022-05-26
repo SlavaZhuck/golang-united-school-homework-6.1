e73d6409c69eb590b8951bb9557642b2c9288b2b
Viachaslau_Zhuk <Zhukviachaslau@gmail.com>
2022-05-26 23:27:19 +0300

    added SumPerimeter method

--- box.go
+++ box.go
@@ -81,8 +81,11 @@ func (b *box) ReplaceByIndex(i int, shape Shape) (Shape, error) {
 
 // SumPerimeter provides sum perimeter of all shapes in the list.
 func (b *box) SumPerimeter() float64 {
-	panic("implement me")
-
+	var sum float64 = 0
+	for _, v := range b.shapes {
+		sum += v.CalcPerimeter()
+	}
+	return sum
 }
 
 // SumArea provides sum area of all shapes in the list.
